e7f3c67eadb8a3e03b44697b20f02b560d93c861
Pete Hunt <pete@smyte.com>
2015-08-25 17:40:48 -0700

    temp
    progress mofo
    
    done

--- spec/webpackRequire.spec.js
+++ spec/webpackRequire.spec.js
@@ -16,6 +16,25 @@ describe('webpackRequire', function() {
     );
   });
 
+  it('has sick stack traces', function(done) {
+    webpackRequire(
+      require('./app/webpack.config.js'),
+      require.resolve('./app/throwError'),
+      function(err, mod) {
+        expect(err).toBe(null);
+        var e = null;
+        try {
+          mod();
+        } catch (_e) {
+          e = _e;
+        }
+
+        expect(e.stack.indexOf('webpack-require/spec/app/throwError.js:1:1)')).toBeGreaterThan(-1);
+        done();
+      }
+    );
+  });
+
   it('shims', function(done) {
     var stateful = require('./app/stateful');
     stateful.value = 'node';
